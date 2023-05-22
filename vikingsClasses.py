c1144e6cc931ebd9de6114274381c2ae44b6efc3
Stefy-Castillo <ecastillolecuona@gmail.com>
2023-05-22 21:34:39 +0200

    [Stefy](lab-vikings)

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,13 +3,42 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+       
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+   
+    def receiveDamage(self, damage):
+        self.health -= damage
+
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking():
+    def __init__(self, health, strength, name):
+        
+        self.name = name
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        
+        self.health -= damage
+        
+        if health > 0:
+            return f"{name} ha recibido {damage} puntos de daño"
+        else:
+            return f"{name} ha muerto en combate"
+    
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # Saxon
 
