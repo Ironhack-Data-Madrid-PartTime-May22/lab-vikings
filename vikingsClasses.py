47a432c7c91ed9868fb431377ef56bf656f586a5
davidfernandez1619 <david.fernandez1619@gmail.com>
2023-05-24 21:13:58 +0200

    all except war

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,19 +3,53 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health -= damage
+        
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strenght):
+        super().__init__(health, strenght)
+        self.name = name
+
+    def receiveDamage(self, damage):
+        super().receiveDamage(damage)
+
+        if self.health > 0:
+            return f'{self.name} has received {damage} points of damage'
+        else: 
+            return f'{self.name} has died in act of combat'
+        
+    def battleCry(self):
+        return 'Odin Owns You All!'
+    
+    
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strenght):
+        super().__init__(health, strenght)
+    
+    def receiveDamage(self, damage):
+        super().receiveDamage(damage)
+
+        if self.health > 0:
+            return f'A Saxon has received {damage} points of damage'
+        else:
+            return f'A Saxon has died in combat'
+    
 
 # War
 
